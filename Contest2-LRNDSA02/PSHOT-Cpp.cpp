b3ea4ecbb1aa978c29958e2a5f2052f62916235e
Nakul Goyal <48354697+nakulGoyal19@users.noreply.github.com>
2020-05-22 15:25:48 +0530

    Create PSHOT-Cpp.cpp

---
+++ Contest2-LRNDSA02/PSHOT-Cpp.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int tc,n;
+	int flag = 0;
+	int c1 = 0,c2 = 0;
+	std::cin >> tc;
+	while(tc--)
+	{
+	    flag = 0;
+	    c1 = 0;
+	    c2 = 0;
+	    std::cin >> n;
+	    string shots;
+	    cin>>shots;
+	    for(int i=0;i<shots.length();i++){
+	        if(i%2==0){
+	            if(shots[i]=='0')
+	                c1+=0;
+	            else
+	                c1+=1;
+	        }else{
+	            if(shots[i]=='0')
+	                c2+=0;
+	            else
+	                c2+=1;
+	        }
+	        int temp = (shots.length()-(i+1))%2==0?(shots.length()-(i+1))/2:(shots.length()-(i+1))/2+1;
+	        if((c1>c2 && (c1-c2)>temp)  || (c1<c2 && (c2-c1)>(((shots.length())-(i+1))/2))){
+	            std::cout << i+1 << std::endl;
+	            flag = 1;
+	            break;
+	        }
+	    }
+	    if(flag==1)continue;
+	    cout<<n*2<<endl;
+	    
+	}
+	return 0;
+}
